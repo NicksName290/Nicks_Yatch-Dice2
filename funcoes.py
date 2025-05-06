e225620127fbf140e7350d589156f3366a9d245f
NicksName290 <nicolasname290@gmail.com>
2025-05-06 15:45:22 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-#versao : 31
+#versao : 33
 import random
 def rolar_dados(qnt):
   lista = []
@@ -173,6 +173,12 @@ def calcula_pontos_quina(l):
       if l.count(i) >= 5:
          return 50
    return 0
+#precisava de soma? E porque nn teve um exercicio pra esse???
+def calcula_pontos_soma(l):
+   soma = 0
+   for i in l:
+      soma += i
+   return soma
 #agora junta tudo aee!! (Eu nn aguento mais)
 def calcula_pontos_regra_avancada(l):
    dic = {}
@@ -182,7 +188,15 @@ def calcula_pontos_regra_avancada(l):
    dic['cinco_iguais'] = calcula_pontos_quina(l)
    dic['full_house'] = calcula_pontos_full_house(l)
    dic['quadra'] = calcula_pontos_quadra(l)
-   dic['sem_combinacao'] = soma
+   dic['sem_combinacao'] = calcula_pontos_soma(l)
    dic['sequencia_alta'] = calcula_pontos_sequencia_alta(l)
    dic['sequencia_baixa'] = calcula_pontos_sequencia_baixa(l)
    return dic
+#Curiosidade, eu passei mais tempo na 1 e na 2 do que todo o resto
+#FAZ JOGADA
+def faz_jogada(lista, categoria, cartela):
+   pontuacao = calcula_pontos_regra_avancada(lista) 
+   for i in cartela:
+      if categoria in cartela[i]:
+         cartela[i][categoria] = pontuacao[categoria]
+   return cartela
