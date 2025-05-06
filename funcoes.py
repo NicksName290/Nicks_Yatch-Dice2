b7f010e993af098d483b50e1ca4962404d0dd0e7
NicksName290 <nicolasname290@gmail.com>
2025-05-06 12:48:56 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-#versao : 13
+#versao : 14
 import random
 def rolar_dados(qnt):
   lista = []
@@ -7,10 +7,18 @@ def rolar_dados(qnt):
     lista.append(valor)
   return lista
 #maybe, im calm
-def guardar_dado(rolados, estoque, guardar):
+def guardar_dado(rolados, estoque, manter):
     lista = []
-    estoque.append(rolados[guardar])
-    del rolados[guardar]
+    estoque.append(rolados[manter])
+    del rolados[manter]
     lista.append(rolados)
     lista.append(estoque)
     return lista
+#ok...
+def remover_dado(rolados, estoque, tirar):
+   lista = []
+   estoque.append(estoque[tirar])
+   del estoque[tirar]
+   lista.append(rolados)
+   lista.append(estoque)
+   return lista
